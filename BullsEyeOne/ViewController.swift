26b284a0d781d5ee04ac74375ecb07e70cd6d4bc
Gospodi <gospodijazzet@gmail.com>
2022-02-17 16:31:15 +0300

    Добавлена отображение значение слайдера при нажатии кнопки

--- BullsEyeOne/ViewController.swift
+++ BullsEyeOne/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    var currentValue: Int = 50
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,23 +18,25 @@ class ViewController: UIViewController {
     
 
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
+        
+        
         let alert = UIAlertController(
             title: "Hello, World!",
-            message: "Это уже не первое моё приложение!",
-            preferredStyle: .alert
-        )
+            message: message,
+            preferredStyle: .alert)
         
         let action = UIAlertAction(
-            title: "Awesome", style: .default, handler: nil
-        )
+            title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
+        
         present(alert, animated: true, completion: nil)
     }
     
     
     @IBAction func sliderMoved(_ slider: UISlider) {
-        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
         
     }
     
