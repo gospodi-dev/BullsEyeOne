e6f983f588804e65de8176dca80c2b7e233f0c71
Gospodi <gospodijazzet@gmail.com>
2022-02-17 19:26:33 +0300

    Добавлен механизм изменения заголовка зависимости от результата игрока

--- BullsEyeOne/ViewController.swift
+++ BullsEyeOne/ViewController.swift
@@ -26,46 +26,51 @@ class ViewController: UIViewController {
     
     func startNewRound() {
         round += 1
-      targetValue = Int.random(in: 1...100)
-      currentValue = 50
-      slider.value = Float(currentValue)
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
         updateLabels()
     }
-
+    
     func updateLabels(){
         targetLabel.text = String(targetValue)
         scoreLabel.text = String(score)
         roundLabel.text = String(round)
     }
-
+    
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
         let points = 100 - difference
-        
         score += points
-
-        let message = "YOU scored \(points) points"
-                     
+        
+        // add these lines
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        let message = "You scored \(points) points"
         
         let alert = UIAlertController(
-            title: "Hello, World!",
+            title: title,  // change this
             message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
-            title: "OK", style: .default, handler: nil)
+            title: "OK",
+            style: .default,
+            handler: nil)
         
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
         
         startNewRound()
-    }
-    
-    
-    @IBAction func sliderMoved(_ slider: UISlider) {
-        currentValue = lroundf(slider.value)
-        
     }
     
     
