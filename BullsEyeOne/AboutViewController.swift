384b0d036ae3e3945d0f49e63e90cf07584422a8
Gospodi <gospodijazzet@gmail.com>
2022-02-18 14:10:55 +0300

    Добавлена отображение HTML файла в Web представлении

--- BullsEyeOne/AboutViewController.swift
+++ BullsEyeOne/AboutViewController.swift
@@ -15,5 +15,16 @@ class AboutViewController: UIViewController {
     @IBAction func close() {
         dismiss(animated: true, completion: nil)
     }
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      if let url = Bundle.main.url(
+        forResource: "BullsEye", withExtension: "html") {
+        let request = URLRequest(url: url)
+        webView.load(request)
+      }
+    }
+
 }
 
